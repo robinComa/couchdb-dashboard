7aa346cb46d4cb83efc0cda7a7bc73d69a0c3e1e
robinComa <robin.coma.public@gmail.com>
2014-12-29 22:13:08 +0100

    form validation

--- app/src/app.js
+++ app/src/app.js
@@ -25,3 +25,23 @@ angular.module('app', [
         $rootScope.user = user;
     });
 });
+
+angular.module('app').directive('ngModelFn', function(){
+    return {
+        require: 'ngModel',
+        scope:{
+            bindModel:'=ngModel'
+        },
+        link: function(scope, elem, attr, ctrl){
+            scope.$watch(function(){
+                return scope.bindModel;
+            }, function(val){
+                try{
+                    ctrl.$setValidity('parse', !val || typeof eval('(' + val + ')') === 'function');
+                }catch(err){
+                    ctrl.$setValidity('parse', false);
+                }
+            });
+        }
+    }
+});
