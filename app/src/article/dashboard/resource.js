bc84ba541ebd3c7acd3979aa23047bd4d1c26f65
robinComa <robin.coma.public@gmail.com>
2015-01-04 19:57:27 +0100

    Update 2015-01-04T18:57:23.573Z

--- app/src/article/dashboard/resource.js
+++
@@ -1,3 +0,0 @@
-angular.module('app').factory('Dashboard', function($pouchDbResource){
-    return new $pouchDbResource('http://robin-db.iriscouch.com/dashboard');
-});
--- app/src/header/controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('app').controller('HeaderCtrl', function($rootScope, $scope, Login){
-
-    $scope.logout = function(){
-        Login.logout();
-        $rootScope.user = null;
-    }
-
-});
