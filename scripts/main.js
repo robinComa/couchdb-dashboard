6a553835fa25e50b2585b1f40232012b35d4c58c
Robin Coma Delperier <rcomadelperier@sqli.com>
2015-01-11 00:13:49 +0100

    Update 2015-01-10T23:13:23.241Z

--- scripts/main.js
+++ scripts/main.js
@@ -189,6 +189,66 @@ angular.module('pouchdb').provider('$pouchDbResource', function(){
     };
 
 });
+angular.module('appMapReduceFn', []).directive('appMapReduceFn', function(){
+    return {
+        require: 'ngModel',
+        scope:{
+            bindModel:'=ngModel'
+        },
+        transclude: true,
+        replace: true,
+        templateUrl: 'src/commons/app-map-fn/view.html',
+        link: function(scope, elem, attr, ctrl){
+
+            var computeRestriction = function(restrictions){
+                return restrictions.map(function(restriction){
+                    return 'doc.' + restriction.key + '.match(/' + restriction.value + '/)[0]';
+                }).join(' && ');
+            };
+
+            var computeFn = function(mapFn){
+                var head = 'function(doc){';
+                var footer = '}';
+                var bodies = [];
+                if(mapFn.restrictions.length > 0){
+                    bodies.push('if(' + computeRestriction(mapFn.restrictions) + '){')
+                }
+                bodies.push('emit(doc.' + mapFn.result.key + ', doc.' + mapFn.result.value + ');')
+                if(mapFn.restrictions.length > 0){
+                    bodies.push('}');
+                }
+                return {
+                    map: [head, '\t' + bodies.join('\n'), footer].join('\n'),
+                    reduce: 'function(k,v){return v.length;}'
+                };
+            };
+
+            scope.mapFn = {
+                result: {
+                    key: '',
+                    value: ''
+                },
+                restrictions: []
+            };
+
+            scope.addRestriction = function(){
+                scope.mapFn.restrictions.push({key: '', value: ''});
+            };
+
+            scope.removeRestriction = function(index){
+                scope.mapFn.restrictions.splice(index, 1);
+            };
+
+            scope.$watch(function(){
+                return scope.mapFn;
+            }, function(val){
+                var mapReduce = computeFn(val);
+                scope.bindModel.map = mapReduce.map;
+                scope.bindModel.reduce = mapReduce.reduce;
+            }, true);
+        }
+    }
+});
 angular.module('appNgModel', []).directive('ngModelFn', function(){
     return {
         require: 'ngModel',
@@ -213,13 +273,14 @@ angular.module('app', [
     'ui.bootstrap',
     'ui.router',
     'ui.sortable',
-    'appLogin',
-    'appNgModel',
     'pascalprecht.translate',
     'infinite-scroll',
     'angular-loading-bar',
     'pouchdb',
-    'googlechart'
+    'googlechart',
+    'appLogin',
+    'appNgModel',
+    'appMapReduceFn',
 ]).config(function($translateProvider, $pouchDbResourceProvider, LoginProvider, config){
 
     $translateProvider.useLoader('$translatePartialLoader', {
@@ -463,11 +524,17 @@ angular.module('app').directive('appAnalyseForm', function(Dashboard){
                 scope.analyse = null;
             };
 
-            scope.show = function(){
-                if(scope.form.$valid){
+            scope.canShow = function(){
+                return scope.form.endPoint.$valid && scope.form.map.$valid;
+            };
+
+            scope.$watch(function(){
+                return scope.analyse;
+            }, function(){
+                if(scope.canShow()){
                     scope.analyseToshow = angular.copy(scope.analyse);
                 }
-            };
+            },true);
 
             scope.submit = function(){
                 if(scope.form.$valid){
